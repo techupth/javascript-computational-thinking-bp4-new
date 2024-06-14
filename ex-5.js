cb77890c9496415440255ef2654b220fa531311d
Wasin <wasin1026@gmail.com>
2024-06-14 23:12:53 +0700

    Exercise #5

--- ex-5.js
+++ ex-5.js
@@ -1,84 +1,48 @@
-let orders = [
-  {
-    id: 1,
-    customerName: "Brenn Hugk",
-    productName: "Chocolate - Milk, Callets",
-    productPrice: 94690,
-    productQuantity: 35,
-    creditCardType: "solo",
-  },
-  {
-    id: 2,
-    customerName: "Celia Dary",
-    productName: "Bread - Pumpernickle, Rounds",
-    productPrice: 10746,
-    productQuantity: 87,
-    creditCardType: "jcb",
-  },
-  {
-    id: 3,
-    customerName: "Toinette Blindermann",
-    productName: "Bar - Granola Trail Mix Fruit Nut",
-    productPrice: 94191,
-    productQuantity: 31,
-    creditCardType: "switch",
-  },
-  {
-    id: 4,
-    customerName: "Anjela Joannet",
-    productName: "Cheese - Parmesan Cubes",
-    productPrice: 5131,
-    productQuantity: 96,
-    creditCardType: "diners-club-carte-blanche",
-  },
-  {
-    id: 5,
-    customerName: "Kennith Bussons",
-    productName: "Wine - White, Pinot Grigio",
-    productPrice: 94432,
-    productQuantity: 75,
-    creditCardType: "jcb",
-  },
-  {
-    id: 6,
-    customerName: "Birdie Shepland",
-    productName: "Tomato Paste",
-    productPrice: 5845,
-    productQuantity: 65,
-    creditCardType: "visa-electron",
-  },
-  {
-    id: 7,
-    customerName: "Willyt Helks",
-    productName: "Asparagus - Green, Fresh",
-    productPrice: 89253,
-    productQuantity: 26,
-    creditCardType: "laser",
-  },
-  {
-    id: 8,
-    customerName: "Bill Champion",
-    productName: "Wine - Red, Harrow Estates, Cab",
-    productPrice: 922100,
-    productQuantity: 40,
-    creditCardType: "china-unionpay",
-  },
-  {
-    id: 9,
-    customerName: "Cris Druce",
-    productName: "Bar Mix - Lemon",
-    productPrice: 83104,
-    productQuantity: 6,
-    creditCardType: "jcb",
-  },
-  {
-    id: 10,
-    customerName: "Darnell O'Currine",
-    productName: "Lime Cordial - Roses",
-    productPrice: 99041,
-    productQuantity: 75,
-    creditCardType: "visa-electron",
-  },
-];
+let studentsScore = {
+  James: 40,
+  Ann: 80,
+  Joe: 52,
+  Ball: 67,
+  Mick: 89,
+  Ole: 10,
+};
 
 // Start coding here
+function average(score)
+{
+   let sum=0;
+   let i=0;
+   for (let key in score)
+    {
+     sum=sum+score[key];
+     i=i+1;
+    }
+ return console.log("Average score is "+(sum/i).toFixed(2));
+}
+function maxScore(score)
+{
+  let max=0;
+  let maxScore="";
+  for(let key in score)
+    {
+      if(score[key]>max){
+        max=score[key]; maxScore=key+ " has the highest score, which is "+max+" points"
+      }
+    }
+    console.log(maxScore);
+}
+function minScore(score)
+{
+  let min=100;
+  let minScore="";
+  for(let key in score)
+    {
+      if(score[key]<min){
+        min=score[key]; minScore=key+ " has lowest score, which is "+min+" points"
+      }
+    }
+    console.log(minScore);
+}
+average(studentsScore);
+maxScore(studentsScore);
+minScore(studentsScore);
